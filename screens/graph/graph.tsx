5ac03ec5459cc3376c8d894c71694fb01e438c71
MetroLeeH <leehe@metro.net>
2024-03-01 13:59:16 -0800

    react native setup

---
+++ screens/graph/graph.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const Graph = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return(
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: 'blue',
+          }}>
+            <Text>HIIIIIIIII</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default Graph;
